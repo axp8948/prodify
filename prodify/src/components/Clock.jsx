dfe1fd806af2981831aa349f8c74e8cf7d9ea8b4
Anmol Pandey <axp8948@mavs.uta.edu>
2025-07-02 10:58:44 -0500

    Mobile Device Optimization for Clock

--- prodify/src/components/Clock.jsx
+++ prodify/src/components/Clock.jsx
@@ -1,6 +1,5 @@
-// src/components/Clock.jsx
-import React, { useState, useEffect } from 'react'
-
+import React from "react"
+import { useState, useEffect } from "react"
 export default function Clock() {
   const [time, setTime] = useState(new Date())
 
@@ -16,8 +15,8 @@ export default function Clock() {
   const seconds = String(time.getSeconds()).padStart(2, '0')
 
   return (
-     <div className="inline-block text-transparent bg-clip-text bg-gradient-to-r from-blue-900 via-purple-500 to-orange-900 px-4 py-2">
-      <span className="font-mono text-2xl">
+    <div className="inline-block shrink text-transparent bg-clip-text bg-gradient-to-r from-blue-900 via-purple-500 to-orange-900 px-2 sm:px-4 py-1 sm:py-2">
+      <span className="font-mono text-base sm:text-lg md:text-xl lg:text-2xl">
         {hours}:{minutes}:{seconds}
       </span>
     </div>
